f35454b28bbe629f1e42ac46fd0bde069df8d15d
daniel1302 <daniel.1302@gmail.com>
2020-01-21 22:55:24 +0000

    Add makefile and empty main file

---
+++ Makefile
@@ -0,0 +1,18 @@
+build : dependencies lint compile
+	
+compile :
+	go build -o deploy main.go 
+	
+run :
+	go run main.go
+
+test : lint compile
+	./deploy --config=./config.yml.dict
+
+lint :
+	golint -set_exit_status main.go
+
+dependencies :
+	go get -u golang.org/x/lint/golint
+	go get -u github.com/aws/aws-sdk-go/aws/session
+	go get -u github.com/aws/aws-sdk-go/service/ec2
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	_ "github.com/aws/aws-sdk-go/aws/session"
+	_ "github.com/aws/aws-sdk-go/service/ec2"
+
+	"fmt"
+	"os"
+)
+
+// InputArgs is struct to keep input arguments
+type InputArgs struct {
+	OldAMI string
+	NewAMI string
+}
+
+func softExit(exitStatus int, message string) {
+	fmt.Println(message)
+	os.Exit(exitStatus)
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		softExit(1, fmt.Sprintf("Invalid usage. Usage: %s OLD_AMI_ID, NEW_AMI_ID", os.Args[0]))
+	}
+
+	inputArgs := InputArgs{os.Args[1], os.Args[2]}
+}
