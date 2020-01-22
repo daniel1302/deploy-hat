a6605adbcc9a90f77f6acf1afcd1b77bb476ee97
daniel1302 <daniel.1302@gmail.com>
2020-01-22 23:48:17 +0000

    Convert some part of main into action handler add run instances handeler

--- actions.go
+++ actions.go
@@ -26,15 +26,43 @@ type ShortInstanceDesc struct {
 	SubnetId string
 	VpcId string
 	SecurityGroupsIds []string
+	Tags map[string]string
 }
 
 type PipelineInfo struct {
+	Version string
 	Input *InputArgs
-	Instances []ShortInstanceDesc
+	OldInstances []ShortInstanceDesc
+	NewInstances []string
+}
+
+type InitializePipelineAction struct {
+	OldAMI string
+	NewAMI string
 }
 
 type ListInstancesAction struct {
 	Svc   *ec2.EC2
+}
+
+type RunInstancesAction struct {
+	Svc   *ec2.EC2
+}
+
+func (this InitializePipelineAction) Commit(pipelineInfo *PipelineInfo) {
+	OldAMI := this.OldAMI
+	NewAMI := this.NewAMI
+	fmt.Println( )
+
+	if OldAMI[:4] != "ami-" || NewAMI[:4] != "ami-" {
+		panic("TODO")	
+	}
+
+	if OldAMI == NewAMI {
+		panic("TODO 1")
+	}
+	
+	pipelineInfo.Input = &InputArgs{OldAMI, NewAMI}
 }
 
 func (this ListInstancesAction) Commit(pipelineInfo *PipelineInfo) {
@@ -54,7 +82,9 @@ func (this ListInstancesAction) Commit(pipelineInfo *PipelineInfo) {
 		panic(err)
 	}
 
+
 	for _, item := range result.Reservations {
+		fmt.Println(item) 
 		instance := item.Instances[0]
 
 		sgIds := []string{}
@@ -63,21 +93,68 @@ func (this ListInstancesAction) Commit(pipelineInfo *PipelineInfo) {
 			sgIds = append(sgIds, *sg.GroupId)
 		}
 
-		pipelineInfo.Instances = append(pipelineInfo.Instances, ShortInstanceDesc{
+		tags := map[string]string{}
+
+		for _, tag := range instance.Tags {
+			tags[*tag.Key] = *tag.Value
+		}
+
+		pipelineInfo.OldInstances = append(pipelineInfo.OldInstances, ShortInstanceDesc{
 			Id: *instance.InstanceId,
 			InstanceType: *instance.InstanceType,
 			KeyName: *instance.KeyName,
 			SubnetId: *instance.SubnetId,
 			VpcId: *instance.VpcId,
 			SecurityGroupsIds: sgIds,
+			Tags: tags,
 		})
 	}
-
-	fmt.Println(pipelineInfo)
 }
 
-func (this ListInstancesAction) Rollback(info *PipelineInfo) {
+func (this ListInstancesAction) Rollback(pipelineInfo *PipelineInfo) {
 	// Nothing to rollback
 }
 
+func (this RunInstancesAction) Commit(pipelineInfo *PipelineInfo) {
 
+	// fmt.Println(pipelineInfo)
+
+	// for _, item := range pipelineInfo.OldInstances {
+
+
+	// 	input := &ec2.RunInstancesInput{
+	// 		ImageId:      aws.String(pipelineInfo.Input.NewAMI),
+	// 		InstanceType: aws.String(item.InstanceType),
+	// 		KeyName:      aws.String(item.KeyName),
+	// 		MaxCount:     aws.Int64(1),
+	// 		MinCount:     aws.Int64(1),
+	// 		SecurityGroupIds: []*string{
+	// 			aws.String(item.SecurityGroupsIds[0]),
+	// 		},
+	// 		SubnetId: aws.String(item.SubnetId),
+	// 		TagSpecifications: []*ec2.TagSpecification{
+	// 			{
+	// 				ResourceType: aws.String("instance"),
+	// 				Tags: []*ec2.Tag{
+	// 					{
+	// 						Key:   aws.String("Name"),
+	// 						Value: aws.String("test-daniel"),
+	// 					},
+						
+	// 				},
+	// 			},
+	// 		},
+	// 	}
+	// 	result, err := this.Svc.RunInstances(input)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Println(result)
+	// 	break
+	// }
+	
+}
+
+func (this RunInstancesAction) Rollback(pipelineInfo *PipelineInfo) {
+	
+}
--- main.go
+++ main.go
@@ -6,23 +6,28 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	// "fmt"
 	// "os"
+	"time"
 )
 
 
 
 func main() {
-
 	sess, err := session.NewSession(&aws.Config{
         Region: aws.String("us-east-1")},
     )
 
 	svc := ec2.New(sess)
-	inputArgs := &InputArgs{"ami-0c2db42c00f8ff366", "ami-0c2db42c00f8ff366"}
-	list := ListInstancesAction{svc}
-	pipelineInfo := &PipelineInfo{Input: inputArgs}
+	pipelineInfo := &PipelineInfo{
+		Version: time.Now().Format("20060102_150405"),
+	}
+	
+
+
 
 	_ = err
-	list.Commit(pipelineInfo)
+	InitializePipelineAction{"ami-0c2db42c00f8ff366", "ami-08724dcf4c591e2f4"}.Commit(pipelineInfo)
+	ListInstancesAction{svc}.Commit(pipelineInfo)
+	RunInstancesAction{svc}.Commit(pipelineInfo)
 	// input := &ec2.DescribeInstancesInput{
 	// 	Filters: []*ec2.Filter{
     //         &ec2.Filter{
