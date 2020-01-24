0b8970018170e24726e0dbf05eacf924f690afc6
Daniel <daniel.1302@gmail.com>
2020-01-24 14:37:51 +0000

    Refactor utils.go

--- src/utils.go
+++ src/utils.go
@@ -1,96 +1,96 @@
 package main
 
 import (
-	"io/ioutil"
-	"net/http"
-	"math/rand"
-	"time"
-	"regexp"
-	"errors"
-	"strings"
-	"fmt"
+    "io/ioutil"
+    "net/http"
+    "math/rand"
+    "time"
+    "regexp"
+    "errors"
+    "strings"
+    "fmt"
 )
 
 
 
 func getClientIP() (string, error) {
-	var kindOfValidIP = regexp.MustCompile(`^([1-9][0-9]{0,2})(\.[0-9]{0,3}){3}$`)
-	apiUrls := []string {
-		"https://api.ipify.org?format=text",
-		"http://myexternalip.com/raw",
-		"https://ident.me/",
-		"http://icanhazip.com",
-		"https://ipecho.net/plain",
-		"https://ifconfig.co/ip",
-	}
+    var kindOfValidIP = regexp.MustCompile(`^([1-9][0-9]{0,2})(\.[0-9]{0,3}){3}$`)
+    apiUrls := []string {
+        "https://api.ipify.org?format=text",
+        "http://myexternalip.com/raw",
+        "https://ident.me/",
+        "http://icanhazip.com",
+        "https://ipecho.net/plain",
+        "https://ifconfig.co/ip",
+    }
 
-	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(apiUrls), func(i, j int) { apiUrls[i], apiUrls[j] = apiUrls[j], apiUrls[i] })
+    rand.Seed(time.Now().UnixNano())
+    rand.Shuffle(len(apiUrls), func(i, j int) { apiUrls[i], apiUrls[j] = apiUrls[j], apiUrls[i] })
 
-	for _, url := range apiUrls {
-		resp, err := http.Get(url)
-		if err != nil {
-			continue
-		}
-		defer resp.Body.Close()
+    for _, url := range apiUrls {
+        resp, err := http.Get(url)
+        if err != nil {
+            continue
+        }
+        defer resp.Body.Close()
 
-		ipBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			continue
-		}
+        ipBytes, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+            continue
+        }
 
-		ip := strings.Trim(string(ipBytes), "\n \t")
-		if  !kindOfValidIP.MatchString(ip) {
-			continue
-		}
+        ip := strings.Trim(string(ipBytes), "\n \t")
+        if  !kindOfValidIP.MatchString(ip) {
+            continue
+        }
 
-		return ip, nil
-	}
+        return ip, nil
+    }
 
-	return "", errors.New("Could not get public IP")
+    return "", errors.New("Could not get public IP")
 }
 
 
 func getHttpResponseCode(url string) (int, error) {
-	client := &http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			return http.ErrUseLastResponse
-		},
-	}
+    client := &http.Client{
+        CheckRedirect: func(req *http.Request, via []*http.Request) error {
+            return http.ErrUseLastResponse
+        },
+    }
 
-	resp, err := client.Get(url)
+    resp, err := client.Get(url)
 
-	if err != nil {
-		return -1, err
-	}
+    if err != nil {
+        return -1, err
+    }
 
-	return resp.StatusCode, nil
+    return resp.StatusCode, nil
 }
 
 func isValidRequest(url string, retries uint) (bool, error) {
 
-	statusCodeProp := 0
-	for retries >  0 {
-		retries--
-		statusCode, err := getHttpResponseCode(url)
-		statusCodeProp = statusCode
+    statusCodeProp := 0
+    for retries >  0 {
+        retries--
+        statusCode, err := getHttpResponseCode(url)
+        statusCodeProp = statusCode
 
-		if err != nil {
-			if retries == 0 {
-				return false, err
-			}
+        if err != nil {
+            if retries == 0 {
+                return false, err
+            }
 
-			continue;
-		}
+            continue;
+        }
 
-		if statusCode >= 200 && statusCode <= 399{
-			return true, nil
-		}
+        if statusCode >= 200 && statusCode <= 399{
+            return true, nil
+        }
 
-		if retries > 0 {
-			time.Sleep(15 * time.Second)
-		}
-	}
+        if retries > 0 {
+            time.Sleep(15 * time.Second)
+        }
+    }
 
-	return false, errors.New(fmt.Sprintf("Response code %d. Expected in <200; 399>", statusCodeProp))
+    return false, errors.New(fmt.Sprintf("Response code %d. Expected in <200; 399>", statusCodeProp))
 }
